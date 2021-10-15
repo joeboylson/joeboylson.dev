63aa97f2b6000d0c00b79e4cf84fbaab6983e631
Joe Boylson <jboylson@saec-kv.com>
2021-10-15 15:12:24 -0400

    refactor: remove ref folder;

--- ref/pages/Project.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Project = () => (
-    <div>
-        <p>project</p>
-    </div>
-);
-
-export default Project;
--- ref/src/pageComponents/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react"
-import { navigate } from 'gatsby';
-
-const IndexPage = () => {
-  useEffect(() => navigate('/creators'))
-  return <span/>;
-};
-
-export default IndexPage;
