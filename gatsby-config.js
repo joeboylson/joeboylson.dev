4cfdcd670c3196d009beefe37728c08d3930471f
Joe Boylson <joeboylson@gmail.com>
2022-01-11 19:51:17 -0500

    feat: add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `public/icon.png`,
+        icon: `public/joeboylson.dev-icon.ico`,
       },
     },
     {
