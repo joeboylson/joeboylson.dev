673b853ce913d9c0a08e90f5c9555e3e13583b0c
Joe Boylson <joeboylson@gmail.com>
2022-01-11 20:12:27 -0500

    fix: not sure why this isn't working - one more try;

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `public/joeboylson.dev-icon.jpg`,
+        icon: `public/joeboylson-dev-icon.jpg`,
       },
     },
     {
