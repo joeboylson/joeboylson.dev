fe8a96704c1940027cc6e574cd089d2b46989bf4
Joe Boylson <joeboylson@gmail.com>
2022-01-11 19:58:25 -0500

    fix: fix gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `public/joeboylson.dev-icon.ico`,
+        icon: `public/joeboylson.dev-icon.jpg`,
       },
     },
     {
